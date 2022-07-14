892f2c1e3c193c5e3ab82c88aee69ca9d2fdf49b
ganwurrt <xiaoming674@163.com>
2022-07-14 15:29:12 +0800

    Update lx.js

--- lx.js
+++ lx.js
@@ -1,11 +1,11 @@
 var body = $response.body;
 
-console.log('body---'+body)
+console.log('body-----'+body)
 function re(reg, str) {
     body = body.replace(reg, str);
 }
 
 var reg1 = /true/g;
 re(reg1, 'false');
-console.log('处理后body---'+body)
+console.log('处理后body---'+body);
 $done(body);
